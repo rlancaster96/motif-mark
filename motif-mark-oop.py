b1ccec968d36342b41e2bbf6f03f28656306bca4
rlancaster96 <rubenl@uoregon.edu>
2024-02-20 14:45:24 -0800

    prase motif and parse fasta functions

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+from __future__ import annotations
+import cairo
+import math
+import bioinfo
+import argparse
+
+# picture of a sequence: positions of exons, introns, and motifs, to scale 
+
 #set up argparse
 def get_args():
     parser = argparse.ArgumentParser(description="Takes a FASTA file with exons denoted in CAPS, and a motif file. Outputs one .png with one image per sequence of motif positions on the sequence.")
@@ -8,3 +16,49 @@ def get_args():
     return parser.parse_args()
 
 args = get_args()
+motiffile = args.motiffile
+fastafile = args.fastafile
+onelinefastafile = "oneline" + fastafile
+
+# define classes 
+
+# read in info from files #
+
+# convert from multiple line fasta to one line fasta #
+bioinfo.oneline_fasta(fastafile, onelinefastafile)
+
+# read in motifs into list # 
+def parse_motif(motiffile: str) -> list:
+    motifs = []
+    with open(motiffile) as fh:
+        for line in fh:
+            line = line.strip()
+            motifs.append(line)
+    return motifs
+
+# read in sequences into list # 
+def parse_fasta(onelinefastafile: str) -> dict:
+    # {sequence : header}
+    sequences = {}
+    with open(onelinefastafile) as fh:
+        for line in fh:
+            line = line.strip()
+            if line.startswith(">"):
+                header = line[1:]
+            else:            
+                sequence = line
+                sequences[sequence] = header
+    return sequences
+
+motifs = parse_motif(motiffile)
+sequences = parse_fasta(onelinefastafile)
+
+
+# degenerate bases 
+
+
+
+
+
+# draw the image 
+
